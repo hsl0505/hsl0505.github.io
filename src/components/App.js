adb147eca7668578e69ea37047404f34c96e4e39
hyunsung <hslgood@gmail.com>
2020-03-16 19:46:07 +0900

    Test webpack-dev-server

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../style.css";
 
 function App() {
-  return <div>portfolio test</div>;
+  return <div>portfolio test!@@@@@@@@@@@@@@@@@@@@@@!!!</div>;
 }
 
 export default App;
