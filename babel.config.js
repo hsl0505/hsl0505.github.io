1baa98d019d9c20cadb87a5ff04e93879ffb6ac7
hyunsung <hslgood@gmail.com>
2020-03-16 17:46:13 +0900

    Init babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,23 @@
+module.exports = function(api) {
+  api.cache(true);
+
+  const presets = [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: {
+          version: 2
+        }
+      }
+    ],
+    "@babel/preset-react"
+  ];
+
+  const plugins = [];
+
+  return {
+    presets,
+    plugins
+  };
+};
