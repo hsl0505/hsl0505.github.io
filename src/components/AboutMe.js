aaee6feac8006a14a5284d349f9503ae153e3a19
hyunsung <hslgood@gmail.com>
2020-03-17 23:48:23 +0900

    Add components

---
+++ src/components/AboutMe.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function AboutMe() {
+  return <div className="AboutMe">aboutme</div>;
+}
+
+export default AboutMe;
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+
+function Navbar() {
+  const initScroll = window.scrollY || document.documentElement.scrollTop;
+  const [scroll, setScroll] = useState(initScroll);
+
+  useEffect(() => {
+    window.addEventListener("scroll", e => {
+      setScroll(window.scrollY || document.documentElement.scrollTop);
+    });
+  }, []);
+
+  return (
+    <div className="Navbar" style={{ top: scroll > 100 ? "20%" : "2%" }}>
+      <ButtonGroup
+        size="large"
+        orientation={scroll > 100 ? "vertical" : "horizontal"}
+      >
+        <Button>Home</Button>
+        <Button>About Me</Button>
+        <Button>Home</Button>
+        <Button>Project</Button>
+        <Button>Contact</Button>
+      </ButtonGroup>
+    </div>
+  );
+}
+
+export default Navbar;
