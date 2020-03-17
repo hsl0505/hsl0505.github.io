97a686edcee89c34595a794b0e03aed7222e2497
hyunsung <hslgood@gmail.com>
2020-03-17 23:48:39 +0900

    Add components at App component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,22 @@
 import React from "react";
 import "../style.css";
 
+import Navbar from "./Navbar";
+import Home from "./Home";
+import AboutMe from "./AboutMe";
+import Project from "./Project";
+import Contact from "./Contact";
+
 function App() {
-  return <div>portfolio test!@@@@@@@@@@@@@@@@@@@@@@!!!</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Home />
+      <AboutMe />
+      <Project />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
