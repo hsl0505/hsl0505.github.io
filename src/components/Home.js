9d910b19bec9a79fe45e787f0a2426a79984ae77
hyunsung <hslgood@gmail.com>
2020-03-19 03:15:59 +0900

    Add text at home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from "react";
 
 function Home() {
-  return <div className="Home">home</div>;
+  return (
+    <div className="Home">
+      <div className="Home_text">
+        <div className="Home_t1">안녕하세요</div>
+        <div className="Home_t2">임현성의 포트폴리오 입니다 :)</div>
+      </div>
+    </div>
+  );
 }
 
 export default Home;
