5e6397c4ec475db35114791e6b8d3f6a7070605e
hyunsung <hslgood@gmail.com>
2020-04-19 08:52:45 +0900

    Add contact

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,32 @@ function Home() {
         <div className="Home_t1">안녕하세요</div>
         <div className="Home_t2">임현성의 포트폴리오 입니다 :)</div>
       </div>
+      <div className="contact">
+        <div className="contact_list">
+          <i class="fas fa-phone fa-2x"></i>
+          <span>010-9309-1797</span>
+        </div>
+        <div className="contact_list">
+          <i class="far fa-envelope fa-2x"></i>
+          <span>hslgood@gmail.com</span>
+        </div>
+        <div className="contact_list">
+          <a href="https://github.com/hsl0505" target="top">
+            <i class="fab fa-github fa-2x contact_link"></i>
+          </a>
+          <a href="https://github.com/hsl0505" target="top">
+            <span>https://github.com/hsl0505</span>​
+          </a>
+        </div>
+        <div className="contact_list">
+          <a href="https://hsl1697.tistory.com" target="top">
+            <i class="fas fa-blog fa-2x contact_link"></i>
+          </a>
+          <a href="https://hsl1697.tistory.com" target="top">
+            <span>https://hsl1697.tistory.com</span>
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
