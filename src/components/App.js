0d487858f396f025401993f46079aaa8e908943d
hyunsung <hslgood@gmail.com>
2020-04-21 01:06:19 +0900

    Add mySkills component

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,15 @@ import React from "react";
 import Home from "./Home";
 import AboutMe from "./AboutMe";
 
+import MySkills from "./MySkills";
+
 function App() {
   return (
     <div className="App">
       <Home />
       <div className="master_text">Master</div>
       <AboutMe />
+      <MySkills />
     </div>
   );
 }
