fbb827b3c6781a0f134643c7024dab0edd1644e8
hyunsung <hslgood@gmail.com>
2020-04-28 02:48:14 +0900

    Add navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Navigation() {
+  return (
+    <ul className="Navigation">
+      <li className="temp">HOME</li>
+      <li>ABOUT</li>
+      <li>PROJECT</li>
+      <li>CONTACT</li>
+      <i className="fas fa-bars fa-2x"></i>
+    </ul>
+  );
+}
