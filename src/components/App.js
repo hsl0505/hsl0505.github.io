56df749d982c773485f314f748ce8b89b8310cab
hyunsung <hslgood@gmail.com>
2020-05-10 18:34:30 +0900

    Add scroll event for sticky nav

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,51 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useState, useEffect } from "react";
 
 import Home from "./Home";
 import Navigation from "./Navigation";
-import AboutMe from "./AboutMe";
-
-import MySkills from "./MySkills";
+import About from "./About";
 
 function App() {
+  const [showNav, setNav] = useState();
+
+  function onScroll() {
+    if (document.documentElement.scrollTop > 1004) {
+      setNav(true);
+    } else {
+      setNav(false);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+  }, [showNav]);
+
   return (
     <Fragment>
       <header>
         <Home />
       </header>
-      <nav>
+      <nav
+        style={
+          showNav
+            ? {
+                position: "fixed",
+                top: "0",
+                width: "100%",
+                animationName: "navAnimation",
+                animationDuration: "1s",
+                animationDirection: "fowards",
+              }
+            : {}
+        }
+      >
         <Navigation />
       </nav>
       <main>
-        <section className="about">test</section>
+        <section className="section_about">
+          <About />
+        </section>
+        <section className="project">플젝</section>
       </main>
-
-      {/* <div className="master_text">Master</div>
-      <AboutMe />
-      <MySkills /> */}
     </Fragment>
   );
 }
