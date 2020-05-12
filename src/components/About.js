8a11c8d01a070ca5fb8c89ce51469aa8953181c2
hyunsung <hslgood@gmail.com>
2020-05-12 16:18:22 +0900

    Add about title, underline

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,28 @@
 import React, { Fragment } from "react";
 
-export default function About() {
+export default function About(props) {
+  const { aboutTitleText, aboutTitleUnder } = props;
+
   return (
     <Fragment>
-      <div className="About_title">About</div>
+      <div className="About_title">
+        <div
+          className="title_text"
+          style={aboutTitleText ? { display: "block" } : {}}
+        >
+          ABOUT
+        </div>
+        <div
+          className="title_underline"
+          style={aboutTitleUnder ? { display: "block" } : {}}
+        ></div>
+      </div>
+      <div className="About_content">
+        <div className="About_myVision">
+          <i className="fas fa-rocket fa-7x"></i>
+          <i className="fas fa-users fa-7x"></i>
+        </div>
+      </div>
     </Fragment>
   );
 }
