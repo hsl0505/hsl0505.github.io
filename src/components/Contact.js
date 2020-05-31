7694ee424a3fbe61365dfc2dd73f404a3a657ed3
hyunsung <hslgood@gmail.com>
2020-05-31 20:33:36 +0900

    Fix contact component

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
             <i className="far fa-envelope fa-3x"></i>
             <span>hslgood@gmail.com</span>
           </li>
-          <li>
+          <li className="secondIcon">
             <i className="fas fa-mobile-alt fa-3x"></i>
             <span>010.9309.1697</span>
           </li>
@@ -22,13 +22,24 @@ export default function Contact() {
         <ul>
           <li>
             <i className="fas fa-blog fa-3x"></i>
-            <span>asjdklasd</span>
+            <span>
+              <a href="https://hsl1697.tistory.com/" target="blank">
+                https://hsl1697.tistory.com/
+              </a>
+            </span>
           </li>
-          <li>
+          <li className="secondIcon">
             <i className="fab fa-github fa-3x"></i>
-            <span>asdfdsf</span>
+            <span>
+              <a href="https://github.com/hsl0505" target="blank">
+                https://github.com/hsl0505
+              </a>
+            </span>
           </li>
         </ul>
+      </div>
+      <div className="copyright_text">
+        hyunsung <span>@2020</span>
       </div>
     </Fragment>
   );
